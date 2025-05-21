fdc4c066b048d54ece7fd60460aa4c7462147968
henok8515 <henokegezew44@gmail.com>
2025-05-21 14:14:46 +0300

    trying the cicd pipline

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-
-
 function App() {
   return (
     <>
-<h1>hello world from aws amplify </h1>
+      <h1>hello world from aws amplify is now update ,{Date.now()} </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
